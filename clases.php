2a7b465c95ac2055538835fd953cd1d81710e9d3
loguic <guilllopez@gmai.com>
2016-11-11 21:40:33 -0500

    construccion
    elaboramos una clase con sus respectivo objeto

--- clases.php
+++ clases.php
@@ -1,6 +1,22 @@
 <?php
 
+class Miclase{
+    public $hola1 = "Variable public";
+    protected $hola2 = "Variable protected";
+    private $hola3 = "Variavle private";
+       
+}
 
+class Miclase2 extends Miclase{
+    
+    public function resultado(){
+        
+        echo $this->hola2;
+    }
+    
+}
 
+$objMiclase2 = new Miclase2();
+$objMiclase2->resultado();
 
 ?>
